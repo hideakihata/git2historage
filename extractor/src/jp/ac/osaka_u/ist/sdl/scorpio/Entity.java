8d483f840289f0bb0707bf20346f27f04c423578
hdrky <hdrky.h@gmail.com>
2011-09-01 00:58:10 +0900

    remove unnecessary files

--- extractor/src/jp/ac/osaka_u/ist/sdl/scorpio/Entity.java
+++
@@ -1,6 +0,0 @@
-package jp.ac.osaka_u.ist.sdl.scorpio;
-
-
-public interface Entity {
-
-}
--- extractor/src/jp/ac/osaka_u/ist/sel/metricstool/pdg/node/PDGDataOutNode.java
+++
@@ -1,5 +0,0 @@
-package jp.ac.osaka_u.ist.sel.metricstool.pdg.node;
-
-public interface PDGDataOutNode {
-
-}
